7a3f82111b20605e3f0cac0a38d04a1b5876e67a
Derek Sawicki <derek.e.sawicki@gmail.com>
2018-03-28 14:43:38 -0400

    attempt to fix collectstatic errors

--- backend/settings.py
+++ backend/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static')
-]
+)
 
 # activate django-heroku
 django_heroku.settings(locals())
