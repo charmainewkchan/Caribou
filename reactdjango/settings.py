9d83311edcb3b0c3b88a1ada5a8740aa6a1e2c3f
Derek Sawicki <derek.e.sawicki@gmail.com>
2018-04-02 16:31:15 -0400

    settings.py config for postgres

--- reactdjango/settings.py
+++ reactdjango/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,6 +84,9 @@ DATABASES = {
     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
   }
 }
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
