e942cbc0d3d733449ca3d176e7ca2e186aed500e
Derek Sawicki <derek.e.sawicki@gmail.com>
2018-04-05 16:28:25 -0400

    user dummy data

--- src/MyProfile/Profile.js
+++ src/MyProfile/Profile.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import '../App.css';
+import user_profile from './profile.json'
 
 class Profile extends Component {
   render() {
     return (
       <div>
-        <h2>Profile Settings</h2>
-        <h3>Name: </h3>
-        <h3>Year: </h3>
-        <h3>College: </h3>
-
+        <h2>Profile Settings</h2> 
+        <h3>Name: </h3><p>{user_profile.name}</p>
+        <h3>Year: </h3><p>{user_profile.year}</p>
+        <h3>College: </h3><p>{user_profile.college}</p>
+        <h3>Description: </h3><p>{user_profile.desc}</p>
       </div>
     );
   }
