bdc1ebab0b39dbd6762ed7fbc870a6ab918cf85e
cwkchan <cwkchan@princeton.edu>
2018-04-08 16:50:29 -0400

    adding new event block

---
+++ src/Events/AddEvent.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import events_data from './events.json';
+import '../App.css';
+
+class AddEvent extends Component {
+  render() {
+    return (
+      <div className= "Events-AddEvents">
+      <div className= "Events-event">
+      <div className="input-group mb-3">
+
+        <input type="text" placeholder="Event Name" aria-label="Event Name" aria-describedby="basic-addon2">
+        </input>
+        <input type="text" className = "Events-description" placeholder="Description" aria-label="Description" aria-describedby="basic-addon2">
+        </input>
+
+      </div>
+
+        <p>Location:___</p>
+        <p>Time:___</p>
+        <p>Capacity: __</p>
+        <button> Create </button>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default AddEvent;
--- src/Events/Events.js
+++ src/Events/Events.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import EventsPanel from './EventsPanel';
 import EventsFilter from './EventsFilter';
+import AddEvent from './AddEvent.js';
 import events_data from './events.json';
 
 import '../App.css';
@@ -31,12 +32,23 @@ class Events extends Component {
       <div>
     	  <p>{this.state.data.test}</p>
 	      <div className="Events row">
+
 	        <div className= "col-3">
 	        <EventsFilter />
 	        </div>
+
 	        <div className= "col-9">
-	      	<EventsPanel events = {events_data}/>
+            <div className= "container" style= {{background:'red'}}>
+              <div className= "row">
+                <AddEvent />
+                </div>
+
+                <div className= "row">
+                <EventsPanel events = {events_data}/>
+                </div>
+            </div>
 	        </div>
+
 	      </div>
       </div>
     );
