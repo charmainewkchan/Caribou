3286c1b3185cf4efe0cb14f36659b7f105f32c24
cwkchan <cwkchan@princeton.edu>
2018-04-08 18:05:47 -0400

    layout changes to new event block

--- src/Events/AddEvent.js
+++ src/Events/AddEvent.js
@@ -7,17 +7,28 @@ class AddEvent extends Component {
     return (
       <div className= "Events-event">
       <div className="input-group mb-3">
-
-        <input type="text" placeholder="Event Name" aria-label="Event Name" aria-describedby="basic-addon2">
+        <input type="text" className = "Events-inputBig" placeholder="Event Name" aria-label="Event Name" aria-describedby="Event Name">
         </input>
-        <input type="text" className = "Events-description" placeholder="Description" aria-label="Description" aria-describedby="basic-addon2">
+      </div>
+      <div className="input-group mb-3">
+        <input type="text" className = "Events-inputSmall" placeholder="Description" aria-label="Description" aria-describedby="basic-addon2">
         </input>
-
       </div>
 
-        <p>Location:___</p>
-        <p>Time:___</p>
-        <p>Capacity: __</p>
+        <div className= "Events-descriptionBox"> Location:
+          <input type="text" className = "Events-inputSmall" placeholder="" aria-label="Location" aria-describedby="location">
+          </input> </div>
+          
+          <div className= "Events-descriptionBox"> Time:
+            <input type="text" className = "Events-inputSmall" placeholder="Start Time" aria-label="StartTime" aria-describedby="StartTime">
+            </input> -
+            <input type="text" className = "Events-inputSmall" placeholder="End Time" aria-label="EndTime" aria-describedby="EndTime">
+            </input>
+            </div>
+
+            <div className= "Events-descriptionBox"> Capacity:
+              <input type="text" className = "Events-inputSmall" placeholder="" aria-label="Description" aria-describedby="basic-addon2">
+              </input> </div>
         <button> Create </button>
       </div>
     );
