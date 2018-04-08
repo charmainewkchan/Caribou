cf401a026ac3d64c3e6cfb9d5fcdd2e53df35008
Derek Sawicki <derek.e.sawicki@gmail.com>
2018-04-08 15:23:23 -0400

    filter checkboxes

--- src/Events/EventsFilter.js
+++ src/Events/EventsFilter.js
@@ -5,12 +5,25 @@ import '../App.css';
 class EventsFilter extends Component {
   render() {
     return (
-      <div className = "row">
-        <div className = "Events-filter">
-        <p> Search: </p>
-        <p> Filter Options:</p>
+        <div className = "Events-filter container">
+          <form>
+            <h2>Filter Panel</h2>
+            <div className="form-group">
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                <label className="form-check-label" for="defaultCheck1">
+                  Tower
+                </label>
+              </div>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+                <label className="form-check-label" for="defaultCheck1">
+                  Colonial
+                </label>
+              </div>
+            </div>
+          </form>
         </div>
-      </div>
     );
   }
 }
