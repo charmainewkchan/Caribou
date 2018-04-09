ea1bb1a1f8723a8c1f8137d3dc2959d220136890
lucyjing <lucyruoyang@gmail.com>
2018-04-09 14:20:32 -0400

    changes to admin.py

--- sampleapp/admin.py
+++ sampleapp/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from sampleapp.models import User
+# from sampleapp.models import User
 from sampleapp.models import PersonalEvent
 from sampleapp.models import ClubEvent
 
 # Register your models here.
-admin.site.register(User)
+# admin.site.register(User)
 admin.site.register(PersonalEvent)
 admin.site.register(ClubEvent)
