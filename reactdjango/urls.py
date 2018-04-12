2c5ed5b678ca0e18c8bfc67238fae2eb384ef485
Derek Sawicki <derek.e.sawicki@gmail.com>
2018-04-12 17:33:44 -0400

    CAS working on login

--- reactdjango/urls.py
+++ reactdjango/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
   path('admin/', admin.site.urls),
   path('api/', include('sampleapp.urls')),
   path('login/', views.login),
-  
+
   re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
 
