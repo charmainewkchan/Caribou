32629f0a4b1e59d5f4563a5945f9fb2e630f14c2
lucyjing <lucyruoyang@gmail.com>
2018-04-13 16:51:49 -0400

    nice

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 	re_path(r'^$', views.test, name='test'),
 	re_path(r'^netid', views.netid, name='netid'),
-	path('user/<netid>/', views.get_user, name='get_user'),
+	path('user/<netid>/', views.get_user, name='get_user'), # retrieve json data for user from netid
 	re_path(r'^post_event', views.post_event, name='post_event'),
 	path('event/<event_id>/', views.get_event, name='get_event'),
 	re_path(r'^get_events', views.get_events, name='get_events'),
