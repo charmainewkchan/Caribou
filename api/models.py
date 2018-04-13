6fc5cb1330c0fce85b1385233620c323f5a9bbcf
lucyjing <lucyruoyang@gmail.com>
2018-04-13 01:09:40 -0400

    added posting events

--- api/models.py
+++ api/models.py
@@ -79,7 +79,7 @@ class User(models.Model):
 		return self.netid
 
 class PersonalEvent(models.Model):
-	event_type = models.CharField(max_length=40)
+	event_type = models.CharField(max_length=40, default="")
 	date_posted = models.DateTimeField(auto_now=True)
 	author = models.ForeignKey('User', on_delete=models.CASCADE,)
 	description = models.TextField('description')
