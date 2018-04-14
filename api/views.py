28234fe959711187552c0c8e09b126f92b24c781
lucyjing <lucyruoyang@gmail.com>
2018-04-13 21:13:33 -0400

    some changes to views like get event join event
    nice

--- api/views.py
+++ api/views.py
@@ -1,40 +1,35 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
 # import datetime
 
 #from django.http import HttpResponse
 from django.shortcuts import redirect
-from api.models import User, PersonalEvent, ClubEvent
+from api.models import User, PersonalEvent, ClubEvent, JoinedEvents
 import json
 from . import CASClient
 
 # Create your views here.
 def test(request):
-	response = {
-		"test": "this is a message from django!"
-	}
-	return JsonResponse(response)
-
+	return HttpResponse("test", status=400)
+#------------------------------------------------------------------------------#
 def get_user(request, netid):
 	user = User.objects.filter(netid=netid)
 	user_json = serializers.serialize('json', user)
 	return HttpResponse(user_json, content_type='application/json')
 
+#------------------------------------------------------------------------------#
 def get_events(request):
 	data = PersonalEvent.objects.all()
 	data_json = serializers.serialize('json', data)
 	return HttpResponse(data_json, content_type='application/json')
 
 def get_event(request, event_id):
-	test = 'event_id is: ' + event_id
-	# event = get_object_or_404(PersonalEvent, pk=event_id)
-	# event_json = serializers.serialize('json', data)
-	# test = {}
-	# test['text'] = 'hi! this works!'
-	# data_json = serializers.serialize('json', test)
-	return HttpResponse(test)
-	# return HttpResponse(event_json, content_type='application/json')
+	event = PersonalEvent.objects.filter(pk=event_id)
+	if (len(event) != 1):
+		return HttpResponse("Event Not Found", status=404)
+	event_json = serializers.serialize('json', event)
+	return HttpResponse(event_json, content_type='application/json')
 
 @csrf_exempt
 def post_event(request):
@@ -53,18 +48,38 @@ def post_event(request):
 	# eating club
 	eating_club = author.eating_club
 	# capacity
-	capacity = data["capacity"]
+	capacity = int(data["capacity"])
 	e = PersonalEvent(author = author, description = description, title = title, location = location, eating_club = eating_club, capacity = capacity)
 	e.save()
 	# event_json = json.loads(request.body)
 	return HttpResponse(e)
 
+@csrf_exempt
+def join_event(request):
+	event_id = int(data["event"])
+	event = PersonalEvent.objects.get(pk=event_id)
+	# check if event full
+	if (event.attendance >= event.capacity):
+		return HttpResponseBadRequest("EVENT FULL", status=400)
+	data_json = json.loads(request.body)
+	data = data_json[0]
+	participant_netid = data["netid"]
+	participant = User.objects.get(netid=participant_netid)
 
+	# increment attendance
+	event.attendance += 1
+	# add to table
+	j = JoinedEvents(participant=participant, event=event)
+	j.save()
+	return HttpResponse(j)
+
+#------------------------------------------------------------------------------#
 def get_club_events(request):
 	data = ClubEvent.objects.all()
 	data_json = serializers.serialize('json', data)
 	return HttpResponse(data_json, content_type='application/json')
 
+#------------------------------------------------------------------------------#
 def netid(request):
 	if 'netid' in request.session:
 		return JsonResponse({'netid': request.session['netid']})
