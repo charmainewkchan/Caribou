3cdad8ccbfe2bb833f4c1f8c8fc7b291c4ec16d3
lucyjing <lucyruoyang@gmail.com>
2018-04-13 22:00:20 -0400

    registered joined events to admin

--- api/admin.py
+++ api/admin.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from api.models import User
 from api.models import PersonalEvent
 from api.models import ClubEvent
+from api.models import JoinedEvents
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(PersonalEvent)
 admin.site.register(ClubEvent)
+admin.site.register(JoinedEvents)
