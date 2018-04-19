e1b2c77626ee4fcedfb26953c934d5c857a5c3d4
Derek Sawicki <derek.e.sawicki@gmail.com>
2018-04-19 15:40:45 -0400

    remove login test from urls

--- api/urls.py
+++ api/urls.py
@@ -16,6 +16,5 @@ urlpatterns = [
 	re_path(r'^join_event', views.join_event),
 	path('hosted_events/<netid>/', views.hosted_events),
 	path('delete_event/<event_id>/', views.delete_event,),
-	path('edit_event/<event_id>/', views.edit_event),
-	path('login_test/<netid>/', views.login_test, name='login_test'),
+	path('edit_event/<event_id>/', views.edit_event)
 ]
