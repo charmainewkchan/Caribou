1fe9a62662934c08c0f81e322af825502dc2522a
cwkchan <cwkchan@princeton.edu>
2018-04-27 10:19:51 -0400

    adding cal

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import EventCal from './EventCal'
 
 import '../App.css';
+
 
 class Home extends Component {
 
@@ -24,10 +26,8 @@ class Home extends Component {
 
           </div>
         </div>
-        <p>Princeton can be a stressful place. Academics are one thing, but social life here can be stressful too, especially when it comes to bicker. Did you ever feel the need to impress someone? To make someone like you after one conversation? Did that stress you out? Bicker is meant to help people get to know each other, but a 10 minute conversation or game won’t really help you do that.
-              Our solution is Bixer, a platform for members of eating clubs to advertise their availability to hang out with underclassmen throughout the year so that when the time comes to bicker, those involved will have had the opportunity to get to know each other in a more laid-back context. Through a system of event postings and email notifications, Bixer will make it simple for users will to connect over activities like pool or eating a meal.
-              It helps you meet people before the intense 3 days of Bicker so that you can get to know people in a more realistic setting. Bridger will also serve to consolidate all Eating Club event information on to one convenient page. Never miss a free sophomore meal again!
-                </p>
+        <p>Here to help you meet people.</p>
+        <EventCal />
       </div>
     );
   }
