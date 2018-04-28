eac9990b07643e5714e0179fe0733761097ab4c8
lucyjing <lucyruoyang@gmail.com>
2018-04-27 21:13:49 -0400

    made some changes

--- api/views.py
+++ api/views.py
@@ -30,7 +30,6 @@ def set_netid(request, netID):
 	g_netid = netID
 	return HttpResponse("good", status=200)
 
-
 #------------------------------------------------------------------------------#
 # HELPER FUNCTIONS #
 # returns list of event id's
@@ -66,8 +65,11 @@ def append_data_to_events(data_json, netid):
 # sends an email with given specs
 def notify(subject, message, tolist):
 	connection =  None
-	for recipient in tolist:
-		tomail = [recipient]
+	for netid in tolist:
+		u = User.objects.get(netid=netid)
+		if len(DoNotMail.objects.filter(user=u)) > 0:
+			continue
+		tomail = [netid+"@princeton.edu"]
 		mail = EmailMultiAlternatives(subject,message,"bixrnoreply@gmail.com", tomail, connection=connection)
 		mail.send()
 
@@ -86,6 +88,20 @@ def get_user(request, netid):
 		return HttpResponse("User Not Found", status=404)
 	user_json = serializers.serialize('json', user)
 	return HttpResponse(user_json, content_type='application/json')
+
+@csrf_exempt
+@casauth
+def toggle_mail(request):
+	netid = get_netid(request)
+	user_set = User.objects.filter(netid=netid)
+	if len(user_set) == 1:
+		user_set.delete()
+		return HttpResponse(netid + " turned on email notifications")
+	else:
+		u = User.objects.get(netid=netid)
+		entry = DoNotMail(user=u)
+		entry.save()
+		return HttpResponse(netod + " removed from mailing list")
 
 @casauth
 def delete_user(request):
@@ -133,10 +149,9 @@ def post_user(request):
 	else: # create a new user
 		u = User(netid=netid, first_name=data["first_name"],last_name=data["last_name"],res_college=data["res_college"],year=data["year"],eating_club=data["eating_club"])
 		u.save()
-		mail = netid + "@princeton.edu"
-		tolist = [mail]
+		tolist = [netid]
 		subject = 'Welcome to Bixr'
-		message = "Hi, " + data["first_name"] + ",\nThanks for joining Bixr and setting up your profile! To opt out of email notifications, visit your Account Settings"
+		message = "Hi " + data["first_name"] + ",\n\nThanks for joining Bixr and setting up your profile! To opt out of email notifications, visit your Account Settings at https://bixr.herokuapp.com/myprofile/account/ \n\n Sincerely,\n The Bixr Team"
 		notify(subject, message, tolist)
 		return HttpResponse("User created " + netid)
 
@@ -252,8 +267,7 @@ def post_event(request):
 		attendees_id = [j.participant.netid for j in joined]
 		tolist = []
 		for netid in attendees_id:
-			mail = netid + "@princeton.edu"
-			tolist.append(mail)
+			tolist.append(netid)
 		subject = 'An event you joined was updated'
 		message = "The event \"" + title + "\" was updated. See the updates at " + WEBSITE + str(event_id) +"/"
 		notify(subject, message, tolist)
@@ -280,7 +294,7 @@ def delete_event(request, event_id):
 	if len(dependencies) > 0:
 		dependencies.delete()
 	# email the attendees
-	tolist = [n+"@princeton.edu" for n in attendees_id]
+	tolist = [n for n in attendees_id]
 	subject = 'An event you joined was deleted'
 	message = "The event \"" + title + "\" that you joined was deleted."
 	notify(subject, message, tolist)
@@ -321,8 +335,7 @@ def join_event(request):
 	j.save()
 	# email event host
 	host = event.author.netid
-	tomail = host + "@princeton.edu"
-	tolist = [tomail]
+	tolist = [host]
 	subject = 'Someone joined your event!'
 	message = "Someone just joined your event \"" + event.title + "\". Check your current guest list at " + WEBSITE + str(event_id) +"/"
 	notify(subject, message, tolist)
@@ -352,8 +365,7 @@ def unjoin_event(request):
 	joined.delete()
 	# email event host
 	host = event.author.netid
-	tomail = host + "@princeton.edu"
-	tolist = [tomail]
+	tolist = [host]
 	subject = 'Someone left your event!'
 	message = "Someone just left your event \"" + event.title + "\". Check your current guest list at " + WEBSITE + str(event_id) +"/"
 	notify(subject, message, tolist)
--- src/MyProfile/EditableProfile.js
+++ src/MyProfile/EditableProfile.js
@@ -7,13 +7,13 @@ import 'react-dropdown/style.css'
 import axios from 'axios';
 
 const years = [2019,2020,2021,2022];
-const eatingclubs = ["Cloister","Tower","none","Colonial","Cannon","Cap","Ivy","TigerInn","Quad","Terrace","Cottage","Charter"]
+const eatingclubs = ["Cloister","Tower","None","Colonial","Cannon","Cap","Ivy","TigerInn","Quad","Terrace","Cottage","Charter"]
 const rescollege = ["Butler", "Wilson", "Mathey", "Rocky", "Whitman","Forbes"]
 
 var eatingClubAbr = {
   "CL":"Cloister",
   "TO":"Tower",
-  "NN":"none",
+  "NN":"None",
   "CO":"Colonial",
   "CN":"Cannon",
   "CA":"Cap",
@@ -28,7 +28,7 @@ var eatingClubAbr = {
 var eatingClubAbrReverse = {
   "Cloister":"CL",
   "Tower":"TO",
-  "none":"NN",
+  "None":"NN",
   "Colonial":"CO",
   "Cannon":"CN",
   "Cap":"CA",
@@ -41,7 +41,7 @@ var eatingClubAbrReverse = {
 }
 
 var rescollegeAbr = {
-  'NN' : "none",
+  'NN' : "None",
   'MA' : "Mathey",
   'RO' : "Rocky",
   'BU' : "Butler",
@@ -52,7 +52,7 @@ var rescollegeAbr = {
 }
 
 var rescollegeAbrReverse = {
-  "none" : 'NN',
+  "None" : 'NN',
 	"Mathey" : 'MA',
 	"Rocky" : 'RO',
 	"Butler" : 'BU',
