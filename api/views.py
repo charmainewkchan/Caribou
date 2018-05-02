8cf7f013e9e89352c530cf0df375167a179c1267
Derek Sawicki <derek.e.sawicki@gmail.com>
2018-05-02 16:53:38 -0400

    offset

--- api/views.py
+++ api/views.py
@@ -195,6 +195,7 @@ def get_events(request):
 
 	dataq = PersonalEvent.objects.all().order_by('pk').filter(eating_club__in=eating_club_filter)
 	num_pages = len(dataq) / page_size
+	offset = page_size * page_num
 	dataq = dataq[offset:(offset+page_size)]
 	data_json = serializers.serialize('json', dataq)
 
