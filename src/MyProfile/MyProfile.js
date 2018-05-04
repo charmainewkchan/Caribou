533d35cbf1332a93dc81107dd3489d37f30bcd7c
Derek Sawicki <derek.e.sawicki@gmail.com>
2018-05-04 12:37:13 -0400

    styling

--- src/MyProfile/MyProfile.js
+++ src/MyProfile/MyProfile.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react';
 import { Switch, Route, NavLink } from 'react-router-dom';
 import Profile from './Profile';
 import Account from './Account';
-import EventsAttending from './EventsAttending';
-import EventsHosting from './EventsHosting';
-
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-
-import DropDownBar from '../DropDownBar'
 
 import '../App.css';
 class MyProfile extends Component {
@@ -22,8 +16,8 @@ class MyProfile extends Component {
   render() {
     return (
 
-      <div className="container events-wrapper MyProfile-width">
-          <div className="MyProfile">
+      <div className="MyProfile-width">
+          <div className="MyProfile events-wrapper ">
           <Profile/>
           <br/>
 
