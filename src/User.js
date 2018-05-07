5e6bc608467f282704d25dbc70e38eb938eea5f7
Derek Sawicki <derek.e.sawicki@gmail.com>
2018-05-06 20:34:41 -0400

    add events list to user page

--- src/User.js
+++ src/User.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios'
 import res_college_map from './res_college_map.json'
-
+import EventsCompactList from './Events/EventsCompactList'
 
 class User extends Component {
 
 	constructor(){
 		super()
     this.state = {
-      fields: {}
+      fields: {},
+      events: []
     }
+
+    this.setEventPage = this.setEventPage.bind(this);
 	}
 
   componentDidMount(){
@@ -24,9 +27,27 @@ class User extends Component {
       this.setState({
         fields: res.data[0].fields
       });
+
+      var get_events_url = "https://bixr.herokuapp.com/api/hosted_events/" + res.data[0].fields.netid + "/"
+
+      axios.post(get_events_url, [])
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          events: res.data
+        })
+      })
+      .catch(err=>console.log(err))
+
     });
+
+
   }
 
+
+  setEventPage(pk) {
+    this.props.history.push('/events/'+pk + "/");
+  }
 
 
   render() {
@@ -41,6 +62,12 @@ class User extends Component {
     	  <div className="user-page-body">
           <p>Class of {this.state.fields.year}</p>
           <p>{res_college_map[this.state.fields.res_college]}</p>
+          
+          <hr/>
+          <h3>Events {this.state.fields.first_name} is Hosting</h3>
+          <div className="user-page-events">
+          <EventsCompactList setEventPage={this.setEventPage} events={this.state.events} />
+          </div>
         </div>
       </div>
       </div>
