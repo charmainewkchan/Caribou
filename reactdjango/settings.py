3497db23e01021703b3da2ff8e07a8a2c4fda8b2
Derek Sawicki <derek.e.sawicki@gmail.com>
2018-05-08 16:02:25 -0400

    remove fun from title, cors switched?

--- reactdjango/settings.py
+++ reactdjango/settings.py
@@ -56,11 +56,12 @@ MIDDLEWARE = [
   'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = (
     'localhost:3000',
 )
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'reactdjango.urls'
 
